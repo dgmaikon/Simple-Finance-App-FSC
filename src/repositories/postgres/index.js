9c3bca72bd248f23be5b1b17b2163942c2742376
Maikon <dgmaikondev@gmail.com>
2024-05-14 14:45:46 -0300

    feat: add PostgresDeleteUserRepository

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -2,3 +2,4 @@ export * from './create-users.js';
 export * from './get-user-by-email.js';
 export * from './get-user-by-id.js';
 export * from './update-users.js';
+export * from './delete-user.js';
